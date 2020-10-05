2d9c42d4e5dc827daecacf3c25da9497db9fcdb4
Victor <vitya.ryabkov@gmail.com>
2020-10-05 23:55:08 +0300

    Feature: add routes

--- server.js
+++ server.js
@@ -5,45 +5,24 @@ const Mongo = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
 const PORT = 6587;
-const DB_URL = `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASS}@contentpedia.k4rqg.mongodb.net/contentpedia_dev?retryWrites=true&w=majority`;
+
+const TestRoutes = require('./routes/TestRoutes');
 
 
 
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
+// Routes
+app.use('/test/', TestRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('hello server');
 });
 
-app.get('/db-test-get', async(req, res) => {
-    const client = new Mongo(DB_URL);
-    await client.connect();
 
-    try {
-        let vdud_docs = await client.db('contentpedia_dev').collection('vdud').find();
-        let result = [];
-
-        await vdud_docs.forEach(element => {
-            console.log(element);
-            result.push(element);
-        })
-
-        res.send(result);
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await client.close();
-    }
-});
-
-app.post('/db-test-post', (req, res) => {
-    console.log(req.body);
-})
-
-
-
+// Launch server
 app.listen(PORT, () => {
     console.log(`Server was started on ${PORT} port.`)
 })
